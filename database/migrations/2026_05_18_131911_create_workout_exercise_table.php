017168ad9bf7659a2830693e626650efb1d43fb9
stevengangue <stevengangue405@gmail.com>
2026-05-18 15:30:39 +0100

    Fix Laravel storage permissions

--- database/migrations/2026_05_18_131911_create_workout_exercise_table.php
+++ database/migrations/2026_05_18_131911_create_workout_exercise_table.php
@@ -6,20 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('workout_exercise', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('workout_id')
+                ->constrained('workouts')
+                ->onDelete('cascade');
+
+            $table->foreignId('exercise_id')
+                ->constrained('exercises')
+                ->onDelete('cascade');
+
+            $table->integer('sets')->default(3);
+            $table->integer('reps')->default(10);
+            $table->integer('rest_time_seconds')->default(60);
+            $table->integer('order_number')->default(0);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('workout_exercise');
